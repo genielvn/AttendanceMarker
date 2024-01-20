4a693cd39dc870630dffe53868988fea86434395
lezzthanthree <arisatominato48@gmail.com>
2024-01-20 20:27:45 +0800

    Added ViewModels

---
+++ ViewModels/SignInViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceMarker.ViewModels
+{
+    class SignInViewModel : ViewBaseModel
+    {
+		private string _username;
+		public string Username
+		{
+			get
+			{
+				return _username;
+			}
+			set
+			{
+				_username = value;
+				OnPropertyChanged(nameof(Username));
+			}
+		}
+	}
+}
