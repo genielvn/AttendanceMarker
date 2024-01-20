bfc206702c85591519c289c08e7a7e9c39879f75
lezzthanthree <arisatominato48@gmail.com>
2024-01-20 20:28:09 +0800

    Revert OnStartup()

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using AttendanceMarker.Models;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,10 @@ namespace AttendanceMarker
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
     }
 
 }
