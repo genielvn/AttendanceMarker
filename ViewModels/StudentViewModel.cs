f7bc11f62a63af962ab04118e9ceeaebfe7a6ce4
lezzthanthree <arisatominato48@gmail.com>
2024-01-31 15:26:44 +0800

    Added Bindings for Students

--- ViewModels/StudentViewModel.cs
+++ ViewModels/StudentViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AttendanceMarker.Models;
 using AttendanceMarker.Views;
 
@@ -11,14 +12,45 @@ namespace AttendanceMarker.ViewModels
     public class StudentViewModel : ViewModelBase
     {
         private IEnumerable<Student> _students;
+
+        private string _subjectName;
+        public string SubjectName
+        {
+            get => _subjectName;
+            set
+            {
+                _subjectName = value;
+                OnPropertyChanged(nameof(SubjectName));
+            }
+        }
+
+        private string _schedule;
+        public string Schedule
+        {
+            get => _schedule;
+            set
+            {
+                _schedule = value;
+                OnPropertyChanged(nameof(Schedule));
+            }
+        }
+        private string _class;
+        public string Class
+        {
+            get => _class;
+            set
+            {
+                _class = value;
+                OnPropertyChanged(nameof(Schedule));
+            }
+        }
+        public ICommand AddStudent { get; }
+        public ICommand StartAttendance { get; }
+
         public StudentViewModel(IEnumerable<Student> students) 
         {
-
+            _students = students;
         }
 
-        public StudentViewModel()
-        {
-
-        }
     }
 }
