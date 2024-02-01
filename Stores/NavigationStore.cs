5cb4993951f5fe195aeb61f629b3e61257ea6767
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-01 16:55:07 +0800

    Stores the current view model

---
+++ Stores/NavigationStore.cs
@@ -0,0 +1,30 @@
+﻿using AttendanceMarker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceMarker.Stores
+{
+    public class NavigationStore
+    {
+        private ViewBaseModel _currentViewModel;
+        public ViewBaseModel CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        public event Action CurrentViewModelChanged;
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
