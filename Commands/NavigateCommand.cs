f84cbf53285c8f476f0e1fc882b06e12b95c1a07
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-02-01 16:54:22 +0800

    Dynamic Navigation Command

---
+++ Commands/NavigateCommand.cs
@@ -0,0 +1,28 @@
+﻿using AttendanceMarker.Models;
+using AttendanceMarker.Stores;
+using AttendanceMarker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceMarker.Commands
+{
+    class NavigateCommand : CommandBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<ViewBaseModel> _createViewModel;
+
+        public NavigateCommand(NavigationStore navigationStore, Func<ViewBaseModel> createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+    }
+}
